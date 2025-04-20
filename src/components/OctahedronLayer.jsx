72975f891e3bf0572421d84e44c2a6800f6e5fc5
NicolasCooper <sevasresident@gmail.com>
2025-04-20 16:21:19 +0500

    remove resium from package.json

--- src/components/OctahedronLayer.jsx
+++ src/components/OctahedronLayer.jsx
@@ -1,26 +1,25 @@
-import { Entity, BoxGraphics } from 'resium'
 import { Cartesian3, Color } from 'cesium'
 
-const OctahedronLayer = ({ data, onClick }) => {
-  // Размер бокса уменьшается с увеличением level
-  const size = Math.max(10, 100 / data.level) // level 1 = 100m, level 5 = 20m
+// const OctahedronLayer = ({ data, onClick }) => {
+//   // Размер бокса уменьшается с увеличением level
+//   const size = Math.max(10, 100 / data.level) // level 1 = 100m, level 5 = 20m
+//
+//   return (
+//     <Entity
+//       name={`Octahedron ${data.id}`}
+//       position={Cartesian3.fromDegrees(
+//         data.position.lon,
+//         data.position.lat,
+//         data.position.height
+//       )}
+//       onClick={onClick}
+//     >
+//       <BoxGraphics
+//         dimensions={new Cartesian3(size, size, size)}
+//         material={Color.RED.withAlpha(0.6)}
+//       />
+//     </Entity>
+//   )
+// }
 
-  return (
-    <Entity
-      name={`Octahedron ${data.id}`}
-      position={Cartesian3.fromDegrees(
-        data.position.lon,
-        data.position.lat,
-        data.position.height
-      )}
-      onClick={onClick}
-    >
-      <BoxGraphics
-        dimensions={new Cartesian3(size, size, size)}
-        material={Color.RED.withAlpha(0.6)}
-      />
-    </Entity>
-  )
-}
-
-export default OctahedronLayer
+// export default OctahedronLayer
